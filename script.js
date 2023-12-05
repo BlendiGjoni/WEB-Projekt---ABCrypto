e07a683e3d3d6f0a9126d7a2ca47d3d8484477db
ArditMirenaa <am69034@ubt-uni.net>
2023-12-05 21:01:50 +0100

    Krijimi i nje Slider-i ne Webfaqe

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
 }
+
+
+let productContainers = [...document.querySelectorAll('.product-container')];
+let nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+let preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
