e3963443357094e4cad569f5bbdee4033f059f8e
ArditMirenaa <am69034@ubt-uni.net>
2024-02-02 15:06:37 +0100

    Png uploads, and file fix

--- assets/PHP/registerController.php
+++ assets/PHP/registerController.php
@@ -1,17 +1,17 @@
 <?php
-    include_once 'userRepository.php';
-    include_once 'user.php';
+    // include_once 'userRepository.php';
+    // include_once 'user.php';
 
-    if(isset($_POST['submit'])){
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $cpassword = $_POST['cpassword'];
-        $dateofbirth = $_POST['dateofbirth'];
+    // if(isset($_POST['submit'])){
+    //     $username = $_POST['username'];
+    //     $email = $_POST['email'];
+    //     $password = $_POST['password'];
+    //     $cpassword = $_POST['cpassword'];
+    //     $dateofbirth = $_POST['dateofbirth'];
 
-        $user = new User($username, $email, $password, $dateofbirth);
-        $userRepository = new userRepository();
+    //     $user = new User($username, $email, $password, $dateofbirth);
+    //     $userRepository = new userRepository();
 
-        $userRepository->insertUser($user);
-    }
+    //     $userRepository->insertUser($user);
+    // }
 ?>
